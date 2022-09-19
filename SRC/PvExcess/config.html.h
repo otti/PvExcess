f7c5b9bc4c25fb6af46f6fd6ad9b2bfe829da8c2
otti <github@ottersberg.name>
2022-09-19 18:50:27 +0200

    Power and time added to config

--- SRC/PvExcess/config.html.h
+++ SRC/PvExcess/config.html.h
@@ -6,45 +6,42 @@ const char sConfigPage[] PROGMEM = R"=====(
 
   <h2>Config PvExcess</h2>
 
-  <form action="/save_new_config_data" method="get">
-    <table border="0">
-      <tr>
-        <td> MQTT Server</td>
-        <td><input id="server" name="server" type="text"></td>
-      </tr>
-      <tr>
-        <td> MQTT Port</td>
-        <td><input id="port" name="port" type="text"></td>
-      </tr>
-      <tr>
-        <td>MQTT User</td>
-        <td><input id="user" name="user" type="text"></td>
-      </tr>
-      <tr>
-        <td>MQTT Pass</td>
-        <td><input id="pass" name="pass" type="password"></td>
-      </tr>
-      <tr>
-        <td>MQTT Topic</td>
-        <td><input id="topic" name="topic" type="text"></td>
-      </tr>
-      <tr>
-        <td>MQTT Key</td>
-        <td><input id="key" name="key" type="text"></td>
-      </tr>
-    </table>
+  <form action="/save_new_config_data" method="get">  
+    MQTT Server<br>
+    <input id="server" name="server" type="text"><br><br>
+
+    MQTT Port<br>
+    <input id="port" name="port" type="text"><br><br>
+
+    MQTT User<br>
+    <input id="user" name="user" type="text"><br><br>
+
+    MQTT Pass<br>
+    <input id="pass" name="pass" type="password"><br><br>
+
+    MQTT Topic<br>
+    <input id="topic" name="topic" type="text"><br><br>
+
+    MQTT Key<br>
+    <input id="key" name="key" type="text"><br><br>
+
+    Power needed by appliance [W]<br>
+    <input id="power" name="power" type="text"><br><br>
+
+    Time to wait before start [s]<br>
+    <input id="time" name="time" type="text"><br><br>
+    
     <input type="submit" value="Save">
   </form>
 
 <script>
 
-  
   function OnLoad()
   {
     const obj = JSON.parse(CurrentValues);
     
-    inputs = document.getElementsByTagName('input'); // Get no of input fielfs
-    len = inputs.length - 1; // The subbmit button is also an input --> - 1
+    inputs = document.getElementsByTagName('input'); // Get no of input fields
+    len = inputs.length - 1; // The submit button is also an input --> - 1
 
     var i = 0;
     for( const x in obj)
